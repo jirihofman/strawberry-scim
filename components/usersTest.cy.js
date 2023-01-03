61a61ef6179e2f48639f57428b5ca4a8267a2948
Jiří Hofman <jiri.hofman@gmail.com>
2023-01-03 22:37:40 +0100

    eslint fix

--- components/usersTest.cy.js
+++ components/usersTest.cy.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import Test from './users'
+import React from 'react';
+import Test from './users';
 
 describe('<Test />', () => {
-  it('renders', () => {
+    it('renders', () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<Test />)
-  })
-})
+        cy.mount(<Test />);
+    });
+});
