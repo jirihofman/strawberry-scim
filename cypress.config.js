3353b6a5a7cabcc42da5a251aa85a514668658f2
Jiří Hofman <jiri.hofman@gmail.com>
2023-09-19 21:37:53 +0200

    Update (#50)
    * chore: update deps
    
    * update next, use port 3000

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     // setupNodeEvents(on, config) {
     //     // implement node event listeners here
     // },
-        baseUrl: 'http://localhost:4041',
+        baseUrl: 'http://localhost:3000',
         specPattern: '**/*.spec.js',
     },
     videoUploadOnPasses: false,
